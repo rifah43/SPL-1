e2aa1177b7e50b44fba5762116434f5f1bbfa2be
Rifah Tashfiha Faria <60043129+rifah43@users.noreply.github.com>
2022-02-10 04:11:53 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<string>tokenList;
+
 int main()
 {
-    char inString[100000];
-    freopen("spl_democode.txt","r",stdin);
-    gets(inString);             //for input from file
+    string inString;
+    int i;
+    //freopen("spl_democode.txt","r",stdin);
+    getline(cin,inString);             //for input from file
     //cout<<inString;
+    string temp = "";
+	for(int i=0;i<inString.length();i++){
+		
+		if(inString[i]==' '){
+			tokenList.push_back(temp);
+			temp = "";
+		}
+		else{
+			temp.push_back(inString[i]);
+		}
+		
+	}
+	tokenList.push_back(temp);
+   for(i=0;i<tokenList.size();i++)
+   {
+           cout << tokenList[i]<<endl;
+   }
     return 0;
 }
