927aca87be71694fbe1cce133de3183affdc641e
Rifah Tashfiha Faria <60043129+rifah43@users.noreply.github.com>
2022-02-09 23:59:58 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char inString[100000];
+    freopen("spl_democode.txt","r",stdin);
+    gets(inString);             //for input from file
+    //cout<<inString;
+    return 0;
+}
