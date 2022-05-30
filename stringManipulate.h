8febebae5d75935147a50afbc5e7c009fbfcc89a
Rifah Tashfiha Faria <60043129+rifah43@users.noreply.github.com>
2022-05-30 19:30:24 +0600

    Delete stringManipulate.h

--- stringManipulate.h
+++
@@ -1,9 +0,0 @@
-#ifndef STRINGMANIPULATE_H
-#define STRINGMANIPULATE_H
-#include<bits/stdc++.h>
-using namespace std;
-
-string convertToString(char* charArray, int size);
-char* subString(char* realStr, int l, int r);
-
-#endif
